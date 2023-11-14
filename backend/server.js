8decbd79f8aba168634f19e2b9f79fffa5a24981
Andie Liu <andieliu@berkeley.edu>
2023-11-13 18:25:24 -0800

    successfully connect to mongodb and save image upon internal post request

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+const process = require('process');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://andieliu:zKgn4y9aaR@receipts.r95pysw.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = process.env.VITE_REACT_APP_DB_CONNECTION_STRING;
+const dbName = 'test';
+// Create a MongoClient 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -10,25 +12,9 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
 const express = require('express');
 const cors = require('cors');
 const Image = require('./models/Image'); // Import the Mongoose model
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -36,21 +22,29 @@ app.use(express.urlencoded({ extended: true }));
 
 const port = 3001; // Choose a port
 
-// Handle GET request for image upload  
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Allows CORS from any origin
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
 
 // Handle POST request for image upload
 app.post('/api/storeImageUrl', async (req, res) => {
   try {
+    // Connect the client to the server
+    await client.connect();
+    // Specify a database to access
+    const db = client.db(dbName);
+    // Reference a particular collection
+    const col = db.collection('images');
     const { imgUrl } = req.body;
 
     // Create a new instance of the Image model
     const image = new Image({ imgUrl });
     console.log(image);
+
     // Save the image to the database
-    await image.save();
+    await col.insertOne(image);
 
     console.log('Image URL saved to the database:', imgUrl);
 
@@ -60,7 +54,6 @@ app.post('/api/storeImageUrl', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
